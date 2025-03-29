366a1215fc68fd56423dc18ee1913fd21e1ae872
ArbertXu <a.xu.wave@gmail.com>
2025-03-29 19:47:45 -0400

    Made Boss, fixed enemy movement, Knockback works?

--- Team7_RogerWilliam/Assets/Scripts/TutorialBossScript.cs
+++ Team7_RogerWilliam/Assets/Scripts/TutorialBossScript.cs
@@ -2,31 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AttackPlayer : MonoBehaviour
+public class TutorialBossScript : MonoBehaviour
 {
     protected GameObject gameHandler;
     protected GameObject player;
 
-    private GameObject floatingText;
-    [SerializeField] private float minMoveSpeed = 1f;
-    [SerializeField] private float maxMoveSpeed = 4f;
-    [SerializeField] private float moveSpeed;
-    [SerializeField] private float attackRange = 2f;
-    [SerializeField] private float attackCooldown = 2f;
-    [SerializeField] private float attackDamage = 10f;
-    
+    protected Rigidbody2D rb2D;
+    [SerializeField] private float moveSpeed = 2.5f;
+
+    [SerializeField] private float attackRange = 4f;
+    [SerializeField] private float attackCooldown = 1f;
+    [SerializeField] private float attackDamage = 20f;
+
+    [SerializeField] public float knockBack = 20f;
     private float distance;
     private float attackTimer;
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.FindWithTag("Player");
+         player = GameObject.FindWithTag("Player");
         if(player == null)
         {
             Debug.Log("ERROR HERE");
         }
+        rb2D = GetComponent<Rigidbody2D>();
         gameHandler = GameObject.FindWithTag("GameController");
-        moveSpeed = Random.Range(minMoveSpeed, maxMoveSpeed);
+     
     }
 
     // Update is called once per frame
@@ -38,12 +39,12 @@ public class AttackPlayer : MonoBehaviour
             transform.position = Vector2.MoveTowards(transform.position, player.transform.position, moveSpeed * Time.deltaTime);
         } else 
         {
-            Debug.Log("MadeItToAttack");
-            currEnemyAttack();
+            Debug.Log("MadeItToAttackBoss");
+            BossAttack();
         }
     }
 
-    protected virtual void currEnemyAttack()
+    protected virtual void BossAttack()
     {
         attackTimer -= Time.deltaTime;
         if (attackTimer <= 0)
@@ -53,6 +54,11 @@ public class AttackPlayer : MonoBehaviour
             {
                 gameHandler.GetComponent<Health>().TakeDamage(attackDamage);
             }
+            player.gameObject.GetComponent<Player_EndKnockBack>().EndKnockBack();
+            Rigidbody2D pushRB = player.gameObject.GetComponent<Rigidbody2D>();
+            Vector2 moveDirectionPush = rb2D.transform.position - player.transform.position;
+            pushRB.AddForce(moveDirectionPush.normalized * knockBack * - 1f, ForceMode2D.Impulse);
+            Debug.Log("KnkockBack happends");
         }
     }
 }
