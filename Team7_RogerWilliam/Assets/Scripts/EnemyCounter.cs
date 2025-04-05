d33fd176bdb1446e10f35a11d01b44d024543fc5
wilkinsonJay <72212972+wilkinsonJay@users.noreply.github.com>
2025-04-05 14:19:16 -0400

    Updated boulder's so it tells you how many kills you need to get past

--- Team7_RogerWilliam/Assets/Scripts/EnemyCounter.cs
+++ Team7_RogerWilliam/Assets/Scripts/EnemyCounter.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class EnemyCounter : MonoBehaviour
 {
     private GameObject[] enemies;
-    private int curr_enemies;
-    private int start_enemies;
     public int goal;
+    private int start_enemies;
+    private int curr_enemies;
     private int kill_count; 
+    private int remaining_kills;
+    public TMP_Text remainingKills_text;
 
     void Start()
     {
@@ -22,10 +25,13 @@ public class EnemyCounter : MonoBehaviour
         enemies = GameObject.FindGameObjectsWithTag("Enemy");
         curr_enemies = enemies.Length;
         kill_count = start_enemies - curr_enemies;
+        remaining_kills = goal - kill_count;
 
         if (goal <= kill_count) {
             GetComponent<BoxCollider2D>().enabled = false;
-            //Debug.Log("Removing boulder collider");
+        }
+        if (remaining_kills >= 0) {
+            remainingKills_text.text = remaining_kills.ToString();
         }
 
     }
