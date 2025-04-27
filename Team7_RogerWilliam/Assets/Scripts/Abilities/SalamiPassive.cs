c83a23c5fcbdc5f6c453729d362618049cfd50b9
danielrjakab1 <daniel.jakab@tufts.edu>
2025-04-27 19:32:09 -0400

    salami speed passive

---
+++ Team7_RogerWilliam/Assets/Scripts/Abilities/SalamiPassive.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SalamiPassive : MonoBehaviour
+{
+    [SerializeField] private AgentMover mover;
+
+    public void DoubleSpeed()
+    {
+        if (mover == null)
+        {
+            Debug.LogError("Mover not assigned!");
+            return;
+        }
+
+        float currentSpeed = mover.moveSpeed;
+        mover.moveSpeed = currentSpeed * 2f;
+    }
+}
