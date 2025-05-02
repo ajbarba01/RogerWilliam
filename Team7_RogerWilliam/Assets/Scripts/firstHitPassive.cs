45029295a4bea1ae7c2ad2336fe9771ee57d4ab2
ArbertXu <a.xu.wave@gmail.com>
2025-05-02 19:48:57 -0400

    Passive for sneak attack. Code made so that the boss has the same loadout as the player but not fully tested yet.

---
+++ Team7_RogerWilliam/Assets/Scripts/firstHitPassive.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class firstHitPassive : Passive
+{
+
+
+    void Start()
+    {
+        Player.Instance.GetComponentInChildren<WeaponHandler>().GetWeapon().onEnemyHit.AddListener(onEnemyHit);
+    }
+
+    void OnDestroy()
+    {
+        Player.Instance.GetComponentInChildren<WeaponHandler>().GetWeapon().onEnemyHit.RemoveListener(onEnemyHit);
+    }
+    void onEnemyHit(Health enemyHealth)
+    {
+        Debug.Log("On hit");
+        if (Mathf.Approximately(enemyHealth.GetHealth(), enemyHealth.GetMaxHealth()))
+        {
+            Debug.Log("Made it to damage");
+            enemyHealth.TakeDamage(5);
+        }
+    }
+}
---
+++ Team7_RogerWilliam/Assets/Scripts/stealPowers.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class stealPowers : MonoBehaviour
+{
+    public LoadoutManager bossLoadout;
+    // Start is called before the first frame update
+    void Start()
+    {
+        setBossLoadoutFromPlayer();
+    }
+
+    // Update is called once per frame
+    void setBossLoadoutFromPlayer()
+    {
+        bossLoadout.SetWeapon(LoadoutManager.Instance.currentWeapon);
+        bossLoadout.SetAbility(LoadoutManager.Instance.currentAbility);
+        bossLoadout.SetPassive(LoadoutManager.Instance.currentPassive);
+    }
+}
