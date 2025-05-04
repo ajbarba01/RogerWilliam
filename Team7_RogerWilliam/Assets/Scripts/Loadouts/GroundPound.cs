0ff9e0d604ea1d13f4b2372d7282dff0b312bccf
wilkinsonJay <72212972+wilkinsonJay@users.noreply.github.com>
2025-05-04 19:44:20 -0400

    Added ground pound ability

---
+++ Team7_RogerWilliam/Assets/Scripts/Loadouts/GroundPound.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundPound : Ability
+{
+    [SerializeField] private float splashArea;
+    [SerializeField] private float knockbackStrength;
+    [SerializeField] private float attackDamage;
+    [SerializeField] public LayerMask enemyLayers;
+    [SerializeField] public LayerMask playerLayers;
+
+    protected override void OnActivate() {
+
+        Collider2D[] hits = Physics2D.OverlapCircleAll(transform.position, splashArea, enemyLayers);
+        foreach (var enemy in hits) {
+            AgentMover hitEnemy = enemy.GetComponent<AgentMover>();
+            Health enemyHealth = enemy.GetComponent<Health>();
+            
+            // damage
+            onEnemyHit.Invoke(enemyHealth);
+            enemyHealth.TakeDamage(attackDamage);
+
+            // knockback
+            Vector2 knockbackDirection = (enemy.transform.position - transform.position).normalized;
+            hitEnemy.ApplyKnockback(knockbackDirection, knockbackStrength);
+        }
+        anim.PlayOnce("Player_Punch");
+    }
+}
